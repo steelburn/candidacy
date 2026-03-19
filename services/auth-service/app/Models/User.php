47463d46982697be860868eecbaa1df44860b993
Khairulnizam Hasan <steelburn@gmail.com>
2026-03-19 13:22:41 +0800

    Phase 1: Migrate auth-service from Laravel Sanctum to Tymon JWT
    - Remove laravel/sanctum from composer.json
    - Remove HasApiTokens trait from User model
    - Delete config/sanctum.php
    - Remove sanctum middleware reference from Kernel.php
    - JWT driver already configured in config/auth.php
    - AuthController already has login/logout/refresh/me methods with JWT

--- services/auth-service/app/Models/User.php
+++ services/auth-service/app/Models/User.php
@@ -6,13 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -66,8 +65,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [
             'tenant_id' => $this->current_tenant_id,
-            'user_id'   => $this->id,
-            'roles'     => $this->roles->pluck('name')->toArray(),
+            'user_id' => $this->id,
+            'roles' => $this->roles->pluck('name')->toArray(),
         ];
     }
 
